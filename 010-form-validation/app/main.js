d403ab94a5601b4622abb9d35f19516950880574
cepa <lukasz.cepowski@xstream.dk>
2013-06-22 15:20:29 +0200

    010 form validation

---
+++ 010-form-validation/app/main.js
@@ -0,0 +1,24 @@
+define([
+	'require',
+	'dojo/ready',
+	'dojox/validate/web'
+], function(require, ready){
+	
+	var app = {};
+	
+	ready(function(){
+		console.log('loaded!');
+	});
+	
+	/**
+	 * Display alert dialog.
+	 */
+	app.alert = function(message){
+		require(['dijit/Dialog'], function(Dialog){
+			var dialog = new Dialog({ title: 'Hello!', content: message });
+			dialog.show();
+		});
+	};
+	
+	return app;
+});
