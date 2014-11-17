a1c3d5ce2ff43843ed26a74811c7cf3e3125d62a
cepa <lukasz@cepowski.com>
2014-11-17 07:17:35 +0100

    fixes

--- fabfile.py
+++ fabfile.py
@@ -1,35 +1,37 @@
-from fabric.api import *
-from fabric.operations import *
-from fabric.contrib.files import *
+import time
+import fabric.api as fapi
+import fabric.contrib.files as ffiles
+#from fabric.api import *
+#from fabric.operations import *
+#from fabric.contrib.files import *
 
-env.project_name = 'dojo-tutorial'
-env.distfile = 'dist.tar.gz'
-env.path = '/var/www'
+fapi.env.project_name = 'dojo-tutorial'
+fapi.env.distfile = 'dist.tar.gz'
+fapi.env.path = '/var/www'
 
-@parallel
-def uptime():
-    run('uptime')
+fapi.env.user = 'root'
+fapi.env.disable_known_hosts = True
 
-@parallel
-def deploy():
-    import time
-    env.release = time.strftime('%Y%m%d%H%M%S')
-    project_path = '%(path)s/%(project_name)s' % env
-    release_path = project_path + '/' + env.release
+@fapi.task
+@fapi.parallel
+def rollout():
+    fapi.env.release = time.strftime('%Y%m%d%H%M%S')
+    project_path = '%(path)s/%(project_name)s' % fapi.env
+    release_path = project_path + '/' + fapi.env.release
     current_path = project_path + '/current'
-    run('mkdir -p ' + release_path)
-    put('%(distfile)s' % env, release_path)
-    run('cd ' + release_path + ' && tar zxf %(distfile)s' % env)
-    if (exists(current_path)):
-        run('cd ' + project_path + ' && unlink current')
-    run('cd ' + project_path + ' && ln -s %(release)s current' % env)
-    run('cd ' + release_path + ' && rm -f %(distfile)s' % env)
+    fapi.run('mkdir -p ' + release_path)
+    fapi.put('%(distfile)s' % fapi.env, release_path)
+    fapi.run('cd ' + release_path + ' && tar zxf %(distfile)s' % fapi.env)
+    if (ffiles.exists(current_path)):
+        fapi.run('cd ' + project_path + ' && unlink current')
+    fapi.run('cd ' + project_path + ' && ln -s %(release)s current' % fapi.env)
+    fapi.run('cd ' + release_path + ' && rm -f %(distfile)s' % fapi.env)
  
-@parallel
+@fapi.task
+@fapi.parallel
 def purge():
-    run('rm -rf %(path)s/%(project_name)s' % env)
+    fapi.run('rm -rf %(path)s/%(project_name)s' % fapi.env)
 
-@parallel
-def restart_phpfpm():
-    run('/etc/init.d/php5-fpm restart')
-
+if 'rollout' in fapi.env.tasks:
+    fapi.local('rm -rf {}'.format(fapi.env.distfile))
+    fapi.local('tar --exclude={} -zcf {} *'.format(fapi.env.distfile, fapi.env.distfile))
